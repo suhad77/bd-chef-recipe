ef2535b73b78b7cbc4907ca0e02b8b1f665b304d
Suhad Mia <suhadmia12@gmail.com>
2023-05-05 16:54:50 +0600

    contact form design complite

--- src/Pages/Home/ContactUs/ContactUs.jsx
+++ src/Pages/Home/ContactUs/ContactUs.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { MdLocationPin } from "react-icons/md";
+import { HiMail, HiPhone } from "react-icons/hi";
 
 const ContactUs = () => {
     return (
@@ -10,12 +12,17 @@ const ContactUs = () => {
                 </div>
             <div className='bg-info'>
                 <Row className='p-5'>
-                    <Col md={4}>
-                        <h3>Hello</h3>
+                    <Col  className='d-flex flex-column justify-content-center' md={4}>
+                        <div>
+                        <h4><MdLocationPin className='me-2 ' style={{fontSize: '25px'}}></MdLocationPin> Chiang Mai in Thailand</h4>
+                        <p><HiMail className='me-2 ' style={{fontSize: '25px'}}></HiMail> contact@email.com</p>
+                        <p><HiPhone className='me-2 ' style={{fontSize: '25px'}}></HiPhone> +00 123 4567</p>
+                        
+                        </div>
                     </Col>
-                    <Col md={8}>
-                        <Form className='w-75'>
-                            <div className="d-flex gap-5">
+                    <Col md={8} className=' my-4'>
+                        <Form className='w-md-75'>
+                            <div className="d-md-flex gap-5">
                                 <Form.Group className="mb-3" controlId="Please Enter Your Name">
                                     <Form.Label>Your Name</Form.Label>
                                     <Form.Control type="name" placeholder="Name" />
@@ -27,7 +34,7 @@ const ContactUs = () => {
                             </div>
                             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                                 <Form.Label>Example textarea</Form.Label>
-                                <Form.Control as="textarea" rows={3} />
+                                <Form.Control className='w-75' as="textarea" rows={3} />
                             </Form.Group>
                             <Button className='px-3' variant="primary">Send</Button>
                         </Form>
